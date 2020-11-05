563aa4d2db49244f76848e4b82f95e562ab41616
Dave Larrimore <dlarrimore@salesforce.com>
2020-11-05 04:34:07 -0500

    updated to using -n for assigning permset

--- byooInit.sh
+++ byooInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -s -f config/project-scratch-def.json
 sfdx force:source:push
-sfdx force:user:permset:assign --permsetname LFO_Demo
+sfdx force:user:permset:assign -n LFO_Demo
 sfdx shane:data:tree:import -p data/export-demo-service_item__c-plan.json -d data/
 sfdx force:org:open
