ae0e38e8f8c756af096df51348473e36711177ba
Dave Larrimore <dlarrimore@salesforce.com>
2020-11-05 04:38:28 -0500

    added delete data

--- byooInit.sh
+++ byooInit.sh
@@ -1,5 +1,6 @@
 sfdx force:org:create -s -f config/project-scratch-def.json
 sfdx force:source:push
 sfdx force:user:permset:assign -n LFO_Demo
+sfdx force:apex:execute -f scripts/apex/deleteData.apex
 sfdx shane:data:tree:import -p data/export-demo-service_item__c-plan.json -d data/
 sfdx force:org:open
