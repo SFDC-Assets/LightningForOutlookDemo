f3c3906ee60446a58dc6821e6c5aad6d3f51e158
Dave Larrimore <dlarrimore@salesforce.com>
2020-11-09 11:03:53 -0500

    Updated Init File

--- byooInit.sh
+++ byooInit.sh
@@ -1,6 +1,15 @@
 sfdx force:org:create -s -f config/project-scratch-def.json
+
 sfdx force:source:push
+
+#Assign PermissionSet
 sfdx force:user:permset:assign -n LFO_Demo
+
+#Delete Existing Data
 sfdx force:apex:execute -f scripts/apex/deleteData.apex
+
+#Import Data
 sfdx shane:data:tree:import -p data/export-demo-service_item__c-plan.json -d data/
+sfdx shane:data:tree:import -p data/export-demo-PCQS_Service_Item__c-plan.json -d data/
+
 sfdx force:org:open
