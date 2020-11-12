ef67142c71260437cd4d97dbd6dbd491cffb011e
Dave Larrimore <dlarrimore@salesforce.com>
2020-11-12 09:13:30 -0500

    Updated

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# Lightning for Outlook
+# Lightning for Outlook Demonstration
 
-# Setup Tasks
+This Demo Scenario.
+
+# Installation
+
+- This does the following...
+
+
+## Setup Tasks
 
 - Create a remote site setting for the community
 - In Communities Settings, "Enable ExperienceBundle Metadata API"
+- Setup o365 Account
+- Link 0365 account to user account
+- add Outlook Flexipage
+
+## Standard Scenario Search Results
+
+If you do not enter any First/Last/or Da
+
+First Name: Jarred
+Last Name: Peplay
+Date of Birth: 02/08/1955
--- force-app/main/default/lwc/outlookServiceItemTile/outlookServiceItemTile.js
+++ force-app/main/default/lwc/outlookServiceItemTile/outlookServiceItemTile.js
@@ -2,4 +2,23 @@ import { LightningElement, api } from 'lwc';
 
 export default class OutlookServiceItemTile extends LightningElement {
     @api serviceItem;
+
+    get serviceItemUrl(){
+      return `/lightning/r/service_item__c/${this.serviceItem.Id}/view`;
+    } 
+
+    get tileTitle(){
+      return `Case #${this.serviceItem.Case_Number__c}`;
+    }
+
+    buttonClick(event) {
+        this.progressValue = event.target.value;
+        // Creates the event with the data.
+        const selectedEvent = new CustomEvent("serviceitembuttonclick", {
+          detail: this.serviceItem
+        });
+    
+        // Dispatches the event.
+        this.dispatchEvent(selectedEvent);
+    }
 }
